f5c33acae2426a1cf3f45eb37131d50fb78f47d8
Trevor Dolby <tdolby@uk.ibm.com>
2021-02-04 15:06:15 +0000

    Switch to mqsipackagebar to allow hybrid minimal builds

--- ace-build.sh
+++ ace-build.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 # Designed for openshift S2I builds; the target directory contains the data passed to the runtime container
+# Modified for the hybrid (combined build.run container)
 
 echo "In ace-build.sh with target directory of $1"
 
 . /opt/ibm/ace-11/server/bin/mqsiprofile
 
-echo "Starting X server for mqsicreatebar"
-Xvfb -ac :99 &
-export DISPLAY=:99
+#echo "Starting X server for mqsicreatebar"
+#Xvfb -ac :99 &
+#export DISPLAY=:99
+#
+#sleep 5
+#
+#echo "Running mqsicreatebar"
+#mqsicreatebar -data . -b /tmp/application.bar -a aceFunction
 
-sleep 5
-
-echo "Running mqsicreatebar"
-mqsicreatebar -data . -b /tmp/application.bar -a aceFunction
+echo "Running mqsipackagebar"
+mqsipackagebar -w . -a /tmp/application.bar -k aceFunction
 
 echo "Unpacking BAR"
 mqsicreateworkdir $1/ace-server
