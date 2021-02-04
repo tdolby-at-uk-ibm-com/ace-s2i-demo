da6eaac87451c57d4403c282f2d06423977c0a93
Trevor Dolby <tdolby@uk.ibm.com>
2021-02-04 15:15:38 +0000

    Update license

--- ace-build.sh
+++ ace-build.sh
@@ -3,6 +3,10 @@
 # Designed for openshift S2I builds; the target directory contains the data passed to the runtime container
 # Modified for the hybrid (combined build.run container)
 
+# This should not be needed, but "oc new-app -e LICENSE=accept" isn't getting through to us.
+# "s2i build -e LICENSE=accept" works fine . . . 
+export LICENSE=accept
+
 echo "In ace-build.sh with target directory of $1"
 
 . /opt/ibm/ace-11/server/bin/mqsiprofile
